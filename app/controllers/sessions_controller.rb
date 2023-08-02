cefee46bc1c4d74fdd02edd6da84dcc1f3a79a5d
tokiwa <r.tokiwa@ingage.jp>
2023-08-02 16:01:46 +0900

    ログインフォームの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
-require 'test_helper'
+class SessionsController < ApplicationController
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get new' do
-    get login_path
-    assert_response :success
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
