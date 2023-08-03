fca16b17b264917fa8f09f95d6f0ca0fdb7450ca
tokiwa <r.tokiwa@ingage.jp>
2023-08-03 14:59:24 +0900

    current_userの重複定義の解消

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,13 +24,6 @@ module SessionsHelper
     end
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
-  def current_user
-    return unless session[:user_id]
-
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
