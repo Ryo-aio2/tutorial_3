32b59c215cdb2337861d09f05c2a0e0e3f48b84b
tokiwa <r.tokiwa@ingage.jp>
2023-08-03 14:43:03 +0900

    ログイン中のみログアウトの処理が走れるように

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Sessions', type: :request do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/sessions/new'
-      expect(response).to have_http_status(:success)
+RSpec.describe "Sessions", type: :request do
+  describe "GET /login" do
+    it "returns http success" do
+      get login_path
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe 'DELETE /logout' do
+    before do
+      user = FactoryBot.create(:user)
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password } }
+    end
+
+    it 'ログアウトできること' do
+      expect(logged_in?).to be_truthy
+      delete logout_path
+      expect(logged_in?).to_not be_truthy
+    end
+
+    it '2回連続でログアウトしてもエラーにならないこと' do
+      delete logout_path
+      delete logout_path
+      expect(response).to redirect_to root_path
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -55,15 +55,4 @@ RSpec.describe 'Users', type: :request do
       end
     end
   end
-
-  describe 'DELETE /logout' do
-    it 'ログアウトできること' do
-      user = FactoryBot.create(:user)
-      post login_path, params: { session: { email: user.email,
-                                            password: user.password } }
-      expect(logged_in?).to be_truthy
-      delete logout_path
-      expect(logged_in?).to_not be_truthy
-    end
-  end
 end
