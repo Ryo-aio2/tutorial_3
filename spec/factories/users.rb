fa874ce8f886bac68d22e173c15d58fba09a6baa
tokiwa <r.tokiwa@ingage.jp>
2023-08-21 10:50:58 +0900

    サンプルユーザーの生成とテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
     admin { false }
+    activated { true }
+    activated_at { '<%= Time.zone.now %>' }
 
     trait :admin_user do
       admin { true }
