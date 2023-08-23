9bde49285e4f622983b3cc73ce00dc68ed52e248
tokiwa <r.tokiwa@ingage.jp>
2023-08-23 16:09:40 +0900

    annotateの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint           not null, primary key
+#  activated         :boolean          default(FALSE)
+#  activated_at      :datetime
+#  activation_digest :string
+#  admin             :boolean          default(FALSE), not null
+#  email             :string           not null
+#  name              :string           not null
+#  password_digest   :string           not null
+#  remember_digest   :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
 
