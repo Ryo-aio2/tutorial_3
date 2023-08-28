e2420f31e6e2476c8d16cefdc8006e0a2edc38ab
tokiwa <r.tokiwa@ingage.jp>
2023-08-28 10:50:11 +0900

    バリデーションの追加,postに字数制限を追加

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -20,5 +20,27 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:micropost) { Micropost.new(content: 'Lorem ipsum', user_id: user.id) }
+
+  it '有効であること' do
+    expect(micropost).to be_valid
+  end
+
+  it 'user_idがない場合は、無効であること' do
+    micropost.user_id = nil
+    expect(micropost).to_not be_valid
+  end
+
+  describe 'content' do
+    it '空なら無効であること' do
+      micropost.content = '    '
+      expect(micropost).to_not be_valid
+    end
+
+    it '141文字以上なら無効であること' do
+      micropost.content = 'a' * 141
+      expect(micropost).to_not be_valid
+    end
+  end
 end
