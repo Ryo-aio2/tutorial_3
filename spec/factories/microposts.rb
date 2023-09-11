f7fef276bd863e29b0dc0f3dcd3a9a2a87f24cbf
Ryo-aio2 <ryo.tokiwa.12@outlook.jp>
2023-09-11 15:18:52 +0900

    rails g controller

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_microposts_on_user_id                 (user_id)
+#  index_microposts_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :orange, class: Micropost do
     content { 'I just ate an orange!' }
